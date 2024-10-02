004ef86b14f38ac1e72160c4acbba7516fe98734
my name <myemailid@domain.com>
2024-10-02 21:09:24 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: '**', component: SignInComponent},
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CognitoService } from '../../services/cognito.service';
+import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageModalComponent } from "../message-modal/message-modal.component";
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MessageModalComponent
+],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
 
+  user: User | undefined;
+  alertMessage: string = '';
+  showAlert: boolean = false;
+
+  constructor(
+    private router: Router,
+    private cognitoService: CognitoService
+  ) {}
+
+  ngOnInit(): void {
+    this.user = {} as User;
+  }
+
+  public signInWithCognito() {
+    if(this.user && this.user.email && this.user.password) {
+      this.cognitoService.signIn(this.user)
+      .then(() => {
+        this.router.navigate(['/']);
+      })
+      .catch((error:any) => {
+        this.displayAlert(error.message)
+      })
+    } else {
+      this.displayAlert("Missing username or password");
+    }
+  }
+
+  private displayAlert(message: string) {
+    this.alertMessage = message;
+    this.showAlert = true;
+  }
 }
